0772bfbbb143bba15d7cf1727f336be1c439d414
tetiana-brekhova <brehovat@gmail.com>
2025-08-10 22:53:56 +0300

    test base

---
+++ fixtures/news_page_fixtures.py
@@ -0,0 +1,22 @@
+import pytest
+from pages.news_page import NewsPage
+
+
+@pytest.fixture
+def send_comment_and_get_comment_data():
+    def _sender_getter(text):
+        page=NewsPage()
+        page.open_page_by_link("https://www.greencity.cx.ua/#/greenCity")
+        #sing_in
+        page.go_to_first_news()
+        page.send_message(text)
+        return page.get_first_comment_data()
+    return _sender_getter
+
+@pytest.fixture
+def get_news_title():
+    page=NewsPage()
+    page.open_page_by_link("https://www.greencity.cx.ua/#/greenCity")
+    page.go_to_first_news()
+    title=page.get_news_title()
+    return title
--- tests/test_send_message.py
+++ tests/test_send_message.py
@@ -1,29 +1,9 @@
 import pytest
-from pages.news_page import NewsPage
-
-
-@pytest.fixture
-def send_comment_and_get_comment_data():
-    def _sender_getter(text):
-        page=NewsPage()
-        page.open_page_by_link("https://www.greencity.cx.ua/#/greenCity")
-        #sing_in
-        page.go_to_first_news()
-        page.send_message(text)
-        return page.get_first_comment_data()
-    return _sender_getter
-
-@pytest.fixture
-def get_news_title():
-    page=NewsPage()
-    page.open_page_by_link("https://www.greencity.cx.ua/#/greenCity")
-    page.go_to_first_news()
-    title=page.get_news_title()
-    return title
+from  fixtures.news_page_fixtures import *
 
 
 def test_check_news_title_text(get_news_title):
-    assert send_comment_and_get_comment_data == 'UpdatedTitleOR'
+    assert get_news_title == 'UpdatedTitleOR'       #change title or link
 
 
 def test_send_message(send_comment_and_get_comment_data):
