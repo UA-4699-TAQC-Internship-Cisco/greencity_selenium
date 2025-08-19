8b850724c7e888d99f2ef1e61fd561761e6b756a
Liubomyr Halamaha <galamagal86@gmail.com>
2025-08-19 14:20:29 +0300

    fix

--- tests/test_navigate_to_another_page_while_editing_news.py
+++ tests/test_navigate_to_another_page_while_editing_news.py
@@ -1,6 +1,5 @@
 import pytest
 
-from pages import BasePage
 from pages.eco_news_list_page import EcoNewsListPage
 from pages.edit_news_page import EditNewsPage
 from pages.header_component import Header
@@ -9,15 +8,13 @@ from pages.news_page import NewsPage
 
 @pytest.mark.edit_news_content
 def test_edit_news_content_field(logged_in_driver):
-    base_page = BasePage(logged_in_driver)
-
     edit_news = EditNewsPage(logged_in_driver)
     edit_news.open_news_page()
 
     news = NewsPage(logged_in_driver)
     news.edit_news_btn()
     edit_news.edit_content_text()
-    logged_in_driver.save_screenshot("edited_text.png")
+    logged_in_driver.save_screenshot("temp_edited_text.png")
     eco_news_btn = Header(logged_in_driver, node=None)
     eco_news_btn.click_eco_news_button()
     edit_news.click_yes_cancel_btn()
