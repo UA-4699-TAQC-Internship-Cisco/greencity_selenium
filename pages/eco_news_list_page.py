c9da057be3890ab6379e543546e566a20e9a9165
tetiana-brekhova <brehovat@gmail.com>
2025-08-20 11:44:35 +0300

    def click_bookmark

--- pages/eco_news_list_page.py
+++ pages/eco_news_list_page.py
@@ -10,6 +10,7 @@ class EcoNewsListPage(BasePage):
     CREATE_NEWS = (By.XPATH, "//div[@id='create-button' and .//span[text()='Create news']]")
     first_news_on_eco_news_page = '//*[@id="main-content"]/div/div[4]/ul/li[1]/a/app-news-list-gallery-view/div/div/div[2]/div[1]/h3'
     ECO_NEWS_TITLE = (By.XPATH, "//h1[@class='main-header']")
+    BOOKMARK_BUTTON = '//*[@id="main-content"]/div/div[1]/div/div/div[2]'
 
     @allure.step("Click 'Create news' button")
     def click_create_news_button(self):
@@ -21,3 +22,8 @@ class EcoNewsListPage(BasePage):
         title_element = self.get_wait().until(EC.presence_of_element_located(self.ECO_NEWS_TITLE))
         actual_text = title_element.text
         assert actual_text == ECO_NEWS_TITLE_TEXT
+
+    @allure.step('Click bookmark')
+    def click_bookmark_button(self):
+        bookmark_button = self.driver.find_element(By.XPATH, self.BOOKMARK_BUTTON)
+        bookmark_button.click()
