c4d5162218b3225bc573fdf43f5a95d908a462eb
tetiana-brekhova <brehovat@gmail.com>
2025-08-26 13:47:55 +0300

    cleaned script

--- tests/test_check_personal_event_icon.py
+++ tests/test_check_personal_event_icon.py
@@ -10,6 +10,6 @@ def test_check_personal_event_icon(login_driver):
     page.click_tag_filter("EVENTS")
     count_users_news = len(page.news_written_by_user(USERNAME))
     page.click_event_icon()
-    count_event_news = page.get_news_items()
+    count_event_news = page.get_news_items_titles()
 
     assert count_event_news == count_users_news
