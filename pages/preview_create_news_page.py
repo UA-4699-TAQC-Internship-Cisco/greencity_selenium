a6abfd7edff94751d1c6b33fd131c6e9a84e0b6b
tetiana-brekhova <brehovat@gmail.com>
2025-08-26 20:50:38 +0300

    added allure

--- pages/preview_create_news_page.py
+++ pages/preview_create_news_page.py
@@ -1,6 +1,5 @@
 import allure
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
 from pages.base_page import BasePage
 
@@ -15,25 +14,30 @@ class PreviewNewsPage(BasePage):
     PRE_TEXT = '//app-news-preview-page/div/div[4]/div[3]/div[2]/div[1]'
 
 
-
+    @allure.step("get author")
     def get_author(self):
         return self.driver.find_element(By.XPATH, self.AUTHOR).text
 
+    @allure.step("'create news' title is present")
     def is_create_news_title_present(self):
         if 'Create news' == self.driver.find_element(By.XPATH, self.PAGE_TITLE).text:
             return True
         else:
             return False
 
+    @allure.step("get title")
     def get_title(self):
         return self.driver.find_element(By.XPATH, self.PRE_TITLE).text
 
+    @allure.step("get link")
     def get_link(self):
         return self.driver.find_element(By.XPATH, self.PRE_LINK).text
 
 
+    @allure.step("get body")
     def get_body_text(self):
         return self.driver.find_element(By.XPATH, self.PRE_TEXT).text
 
+    @allure.step("get tags")
     def get_tags(self):
         return  self.driver.find_element(By.XPATH, self.PRE_TAGS).text
