de8e53f5e54aa2aa35d6625423882288f05ac4ae
tetiana-brekhova <brehovat@gmail.com>
2025-08-27 19:08:56 +0300

    test precommit

--- tests/test_preview_create_news-page.py
+++ tests/test_preview_create_news-page.py
@@ -3,14 +3,16 @@ from pages.eco_news_list_page import EcoNewsListPage
 
 
 def test_check_preview_page_button(login_driver):
-    """Test to verify that the preview page displays the correct information
-    after entering news details on the create news page."""
+    """
+    Test.
 
+    Check the accuracy of the information in the sent message.
+    """
     news_title = "This is a test news title"
     news_link = "https://www.greencity.cx.ua/#/greenCity"
-    body_text = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+    body_text = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
                 ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris \
-                nisi ut aliquip ex ea commodo consequat.'''
+                nisi ut aliquip ex ea commodo consequat."""
 
     preview_page = (
         EcoNewsListPage(login_driver)
@@ -22,7 +24,8 @@ def test_check_preview_page_button(login_driver):
         .press_tag("Education")
         .enter_link(news_link)
         .enter_body_text(body_text)
-        .click_preview_button())
+        .click_preview_button()
+    )
 
     title = preview_page.get_title()
     tags = preview_page.get_tags()
@@ -32,7 +35,7 @@ def test_check_preview_page_button(login_driver):
 
     assert preview_page.is_create_news_title_present()
     assert title == news_title
-    assert f'by {EXPECTED_USERNAME}' == author
+    assert f"by {EXPECTED_USERNAME}" == author
     assert body == body_text
     assert link == news_link
     assert "News" in tags
--- tests/test_send_message.py
+++ tests/test_send_message.py
@@ -1,4 +1,7 @@
+from config.resources import EXPECTED_USERNAME
+
 
 def test_send_message(send_comment_and_get_comment_data):
+    """Test the accuracy of the information in the sent message."""
     comment_data = send_comment_and_get_comment_data("some valid text for the test")
-    assert comment_data == {"author": "user from .env", "text": "some valid text for the test"}  # change user
+    assert comment_data == {"author": EXPECTED_USERNAME, "text": "some valid text for the test"}  # change user
