4ca37cdc7792ff97bc7d9d0166e6805addbb51f8
tetiana-brekhova <brehovat@gmail.com>
2025-08-27 19:29:36 +0300

    fix docstrings

--- tests/test_eco_news_filter_by_tags.py
+++ tests/test_eco_news_filter_by_tags.py
@@ -1,15 +1,17 @@
 import pytest
+
 from pages.eco_news_list_page import EcoNewsListPage
 
 
 @pytest.mark.eco_news_filter_by_tags
 def test_eco_news_filter_by_tags(login_driver):
+    """Check tag filters on eco news page."""
     page = EcoNewsListPage(login_driver).get_header().click_eco_news_button()
     tags = ["NEWS", "EVENTS", "EDUCATION", "INITIATIVES", "ADS"]
     # step_1
     for tag in tags:
         page.click_tag_filter(tag)
-        all_tags = [item for element in page.get_all_tags_after_press_tag() for item in element.split('|\n')]
+        all_tags = [item for element in page.get_all_tags_after_press_tag() for item in element.split("|\n")]
         assert all_tags.count(tag) == page.get_news_count_from_string()
         assert page.is_tag_filter_active(tag)
         page.click_tag_filter(tag)
